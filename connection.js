df60b4326fb974c7fd25340ba3276ac2346de79e
Rhithika-Sree <94616126+Rhithika-Sree@users.noreply.github.com>
2022-05-30 12:00:17 +0530

    Update connection.js

--- connection.js
+++ connection.js
@@ -1,11 +1,11 @@
 const {Client} = require('pg')
 
 const client = new Client({
-    host: "ec2-54-204-56-171.compute-1.amazonaws.com",
-    user: "xjcwgqixvmaxip",
+    host: "ec2-44-196-174-238.compute-1.amazonaws.com",
+    user: "zdcbncrxibvvvg",
     port: 5432,
-    password: "1f92a66c9e7d334525fe4f7f4b6e2e105289048b9044c4cb3a3569090d3596fa",
-    database: "datfet0269ik1s",
+    password: "855bde4c53798c19f4ddbbbfb636d5bc4800e047f3bd81141e6e16b1d29d55c4",
+    database: "d2gqv6omcd45u2",
     ssl:{
         rejectUnauthorized: false
     }
